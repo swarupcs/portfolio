f02bba73e5e64cd567e433d6fa4836a71e193188
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-12-25 15:32:49 +0530

    Complete NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Main from './Components/Main';
 import About from './Components/About';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className='flex flex-col overflow-x-hidden'>
+    <div className='absolute w-screen'>
+      <Navbar/>
+    </div>
       <div className='min-h-screen'>
         <div className='absolute'>
             <Main/>
