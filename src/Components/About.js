8d0a49118dd3a312af3d9159824b60605c101aa8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-12-25 14:39:32 +0530

    Add Resume button and Profile

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import Profile from '../assests/profile.png';
+import React from "react";
+import Profile from "../assests/profile.png";
 import { Styles } from "../utils/Style";
 import Resume from "../assests/resume.pdf";
+import { TiArrowDownOutline } from "react-icons/ti";
 
 function About() {
   return (
-    <div className='flex flex-wrap justify-center items-center mt-10 p-10 ' id="Home">
+    <div
+      className="flex flex-wrap justify-center items-center mt-10 p-10 "
+      id="Home"
+    >
+      <div className="flex-1 py-4">
+        <span className={Styles.heroHeadText}>
+          Hi, I'm <span className="text-orange-600"> Swarup Das</span>
+          <br />
+          <span className={Styles.heroSubText}>
+            I am a MERN Stack Developer &
+            <br />
+            Web Designer
+          </span>
+        </span>
 
-        <div className='flex-1 py-4'>
-            <span className={Styles.heroHeadText}>
-                Hi, I'm <span className='text-orange-600'> Swarup Das
-
-                </span>
-                <br />
-                <span className={Styles.heroSubText}>
-                    I am a MERN Stack Developer & 
-                    <br />
-                    Web Designer
-                </span>
-            </span>
+        <br />
+        <div className="w-[14rem]">
+          <a href={Resume} download="Resume" target="blank" rel="noreferrer">
+            <div className="p-4 mt-2 w-[13rem] rounded-xl text-orange-500 flex justify-center border-2 border-orange-500 hover:border-violet-500 shadow-lg hover:shadow-pink-500 ">
+              <TiArrowDownOutline className="text-lg mr-3 mt-1 text-orange-500">
+                {" "}
+              </TiArrowDownOutline>{" "}
+              Resume
+            </div>
+          </a>
         </div>
-      
+      </div>
+
+      <div  className='flex flex-wrap' >
+            <img src={Profile} alt='profile' className='element object-contain p-4'/>
+
+        </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
